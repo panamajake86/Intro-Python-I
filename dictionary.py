7a3caad3e5ed196d5c547ce865dbc1bb83f0c5ae
Jake <panamajake86@gmail.com>
2020-07-08 19:37:27 -0600

    Just finished the Hello World section of the homework.

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+#create an empy dictionary
+
+d = {}
+
+dict2 = {"foo": 12, "bar": 20}
+
+#print and access elements in dictionary
+print(dict2["bar"])
+
+#iterate through dictionary
+for k in dict2:
+    print(f'{k} is {dict2[k]}')
