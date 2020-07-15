ae3317fd2ef1c98eecf5f2a016815890f72188f2
Jake <panamajake86@gmail.com>
2020-07-14 21:15:45 -0600

    Finished everything, done and dusted.

--- src/03_modules.py
+++ src/03_modules.py
@@ -31,14 +31,18 @@ import os
 
 # Print the current process ID
 # YOUR CODE HERE
+pid = os.getpid()
 
-print(getpid(os))
+print(pid)
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+cwd = os.getcwd()
 
-
+print(cwd)
 
 # Print out your machine's login name
 # YOUR CODE HERE
+login = os.getlogin()
 
+print(login)
